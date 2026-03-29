17d013168aae3e08652cd48a0895da216a7c042c
Goncalo <gfs.oliveira@campus.fct.unl.pt>
2026-03-29 22:19:31 +0100

    final

--- src/main/java/pt/unl/fct/di/adc/firstwebapp/util/auth/AuthenticatedRequest.java
+++
@@ -1,8 +0,0 @@
-package pt.unl.fct.di.adc.firstwebapp.util.auth;
-
-public class AuthenticatedRequest {
-    public Object input;
-    public AuthToken token;
-
-    public AuthenticatedRequest() { }
-}
